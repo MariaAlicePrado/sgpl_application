e71c561765fc96b1bb6b6cc6935e88cabed24aaa
MariaAlicePrado <mariaalice.ap2006@gmail.com>
2025-06-04 11:47:35 -0300

    commit 1

--- lib/pages/Historico.dart
+++ lib/pages/Historico.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:sgpl_application/pages/Aviso.dart';
 
 class Historico extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SGPLInfoPage(),
+    return Scaffold(
+      appBar: AppBar(title: Text('HISTÓRICO')),
+
+      // Corpo da tela com botão centralizado
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print('Botão pressionado');
+          },
+          child: Text('Clique aqui'),
+        ),
+      ),
+
+      // Menu inferior fixo com dois IconButton
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  print('Home clicado');
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  print('Configurações clicado');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

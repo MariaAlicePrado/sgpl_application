3663ca85cf769ef0bf1ee8fd68245a0511ec44f5
MariaAlicePrado <mariaalice.ap2006@gmail.com>
2025-06-04 11:40:10 -0300

    primeiro commit

---
+++ lib/pages/Historico.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sgpl_application/pages/Aviso.dart';
+
+class Historico extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SGPLInfoPage(),
+    );
+  }
+}
